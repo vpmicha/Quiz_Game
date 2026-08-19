c917099c119f28167de139043ac709c139046dcd
vpmicha <vpmicha@gmail.com>
2026-08-19 22:08:06 +0300

    fixed some bags regarding scope of lists and the number of questions asked

--- Quiz.py
+++ Quiz.py
@@ -3,19 +3,22 @@ import random
 from questions import questionsDict
 from datetime import date
 
-used_questions = []
-
 def main():
+    used_questions = []
+    questions10 = []
+    answers10 = []
     questionsList = list(questionsDict)
-    questions10 =get_10_questions(questionsList, questions10)
+    questions10 = get_10_questions(questionsList, questions10, used_questions)
     answers10 = ask_10_questions(questions10, answers10)
 
-def get_10_questions(qList, q10):
-    for _ in range(10):
+def get_10_questions(qList, q10, used_q):
+    while True:
         q = random.choice(range(len(qList)))
-        if q not in used_questions:
+        if q not in used_q:
             q10.append(qList[q])
-            
+            used_q.append(q)
+            if len(q10) == 10:
+                break
         else:
             continue
     return q10
@@ -25,12 +28,3 @@ def ask_10_questions(q10, a10):
         print(question)
         a10.append(input('Answer: ').strip().lower())
     return a10
-
-
-
-
-
-
-
-
-
