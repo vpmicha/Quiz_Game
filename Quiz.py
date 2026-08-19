0eead3dfdb5653933e02d2dbd6780c564781eac2
vpmicha <vpmicha@gmail.com>
2026-08-19 22:01:57 +0300

    added an ask_10_questions function that asks the 10 questions, recieves an answer from user, appends it in a list and returns the list

--- Quiz.py
+++ Quiz.py
@@ -3,23 +3,31 @@ import random
 from questions import questionsDict
 from datetime import date
 
-questions10 = []
 used_questions = []
 
 def main():
     questionsList = list(questionsDict)
-    get_10_questions(questionsList, questions10)
+    questions10 =get_10_questions(questionsList, questions10)
+    answers10 = ask_10_questions(questions10, answers10)
 
 def get_10_questions(qList, q10):
     for _ in range(10):
         q = random.choice(range(len(qList)))
         if q not in used_questions:
             q10.append(qList[q])
+            
         else:
             continue
     return q10
 
-        
+def ask_10_questions(q10, a10):
+    for question in q10:
+        print(question)
+        a10.append(input('Answer: ').strip().lower())
+    return a10
+
+
+
 
 
 
