18078950880866ea36b10137f9d75eb8de4babe8
vpmicha <vpmicha@gmail.com>
2026-08-20 03:47:35 +0300

    implemented a validation function that checks the correctness of the answers given by the player

--- Quiz.py
+++ Quiz.py
@@ -4,12 +4,17 @@ from questions import questionsDict
 from datetime import date
 
 def main():
-    used_questions = []
-    questions10 = []
-    answers10 = []
-    questionsList = list(questionsDict)
-    questions10 = get_10_questions(questionsList, questions10, used_questions)
-    answers10 = ask_10_questions(questions10, answers10)
+    retry = False
+    while retry:
+        used_questions = []
+        questions10 = []
+        answers10 = []
+        questionsList = list(questionsDict)
+        questions10 = get_10_questions(questionsList, questions10, used_questions)
+        answers10 = ask_10_questions(questions10, answers10)
+        score = validate(questions10, answers10)
+        print(f'{score}/10')
+
 
 def get_10_questions(qList, q10, used_q):
     while True:
@@ -28,3 +33,14 @@ def ask_10_questions(q10, a10):
         print(question)
         a10.append(input('Answer: ').strip().lower())
     return a10
+
+def validate(q10, a10):
+    score = 0
+    for a, q in zip(a10, q10):
+        if a == questionsDict[q]:
+            print('Correct')
+            score += 1
+        else:
+            print('Incorrect')
+            score = score
+    return score
